bf09f929b66541c65d8bba8e244e2abbff466a1b
katogory <145012865+Katogory@users.noreply.github.com>
2025-09-22 20:12:11 +0400

    refractor: simplify reverseString using split, reverse, join

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,9 +1,5 @@
 const reverseString = function(word) {
-    let arr = [];
-    for (let i = 0; i < word.length; i++) {
-        arr.unshift(word.slice(i, i + 1));
-    }
-    return arr.join("");
+   return word.split('').reverse().join('');
 };
 
 // Do not edit below this line
