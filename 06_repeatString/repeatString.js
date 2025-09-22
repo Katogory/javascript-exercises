ba169425f3428c5d01f70de60b14311c74497e64
katogory <145012865+Katogory@users.noreply.github.com>
2025-09-22 19:23:46 +0400

    feat: add repeatString function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, amount) {
+    let combinedString = "";
+    if (amount < 0) {
+        return "ERROR";}
 
+    for (let i = 0; i < amount; i++) {
+        combinedString += word;
+    }
+    return combinedString;
 };
 
 // Do not edit below this line
