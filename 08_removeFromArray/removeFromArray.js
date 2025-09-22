76f61ed21054c0077d49af92c23399e0fe5c5982
katogory <145012865+Katogory@users.noreply.github.com>
2025-09-22 22:43:20 +0400

    feat: complete removeFromArray function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    return array.filter(el => theArgs.includes(el) ? false : true);
 };
 
 // Do not edit below this line
