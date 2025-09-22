35d47a0329f11ba51d29c64093e7c331cec5f5b9
katogory <145012865+Katogory@users.noreply.github.com>
2025-09-22 20:07:07 +0400

    add reverseString function

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let arr = [];
+    for (let i = 0; i < word.length; i++) {
+        arr.unshift(word.slice(i, i + 1));
+    }
+    return arr.join("");
 };
 
 // Do not edit below this line
